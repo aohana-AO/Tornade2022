16a808cb50a7b015069f2114987d449a472598e9
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-08-17 01:53:48 +0900

    ⚡️ install mui

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    modularizeImports: {
+      '@mui/icons-material': {
+        transform: '@mui/icons-material/{{member}}',
+      },
+      '@mui/material': {
+        transform: '@mui/material/{{member}}',
+      },
+    },
+  },
   reactStrictMode: true,
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { defaultTheme } from '~/styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
